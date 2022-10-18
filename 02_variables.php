a192d637b4f39067d3b8c51b2610a6dc1e125da4
dcosovan <david.cosovan@leviatan.ro>
2022-10-18 09:34:30 +0300

    lec 1

---
+++ 02_variables.php
@@ -0,0 +1,39 @@
+<?php
+
+// What is a variable
+
+// Variable types
+
+// Declare variables
+$name = 'michael';
+$age = 3;
+$male = true;
+
+// Print the variables. Explain what is concatenation
+
+//echo $name.'<br>';
+//echo $age.'<br>';
+// echo $male.'<br>';
+
+// Print types of the variables
+echo gettype($name).'<br>';
+// Print the whole variable
+var_dump($name);
+// Change the value of the variable
+
+// Print type of the variable
+
+// Variable checking functions
+
+//echo var_dump(is_int($age));
+
+// Check if variable is defined
+
+//isset($addres);
+//var_dump(isset($addres));
+
+// Constants
+
+//define('pi',3.14);
+//echo pi;
+// Using PHP built-in constants
---
+++ 03_numbers.php
@@ -0,0 +1,23 @@
+<?php
+
+// Declaring numbers
+$a = 5;
+$b = 4;
+$c = 1.2;
+// Arithmetic operations
+
+// Assignment with math operators
+
+// Increment operator
+
+// Decrement operator
+
+// Number checking functions
+
+// Conversion
+
+// Number functions
+
+// Formatting numbers
+
+// https://www.php.net/manual/en/ref.math.php

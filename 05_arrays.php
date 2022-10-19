691f6e2a33b6e9d7f1db5e36b1a7e157d93d4e69
dcosovan <david.cosovan@leviatan.ro>
2022-10-19 16:37:05 +0300

    lec4

--- 05_arrays.php
+++ 05_arrays.php
@@ -1,57 +1,85 @@
 <?php
 
-// Create array
+// // Create array
 
-$fruts = ["banana", "apple", "orange"];
+// $fruts = ["banana", "apple", "orange"];
 
-// Print the whole array
+// // Print the whole array
 
- echo '<pre>';
- var_dump($fruts);
- echo '</pre>';
+//  echo '<pre>';
+//  var_dump($fruts);
+//  echo '</pre>';
 
-// Get element by index
+// // Get element by index
 
-echo $fruts[0].'<br>';
+// echo $fruts[0].'<br>';
 
-// Set element by index
+// // Set element by index
 
-// Check if array has element at index 2
+// // Check if array has element at index 2
 
-// Append element
-
-// Print the length of the array
-
-// Add element at the end of the array
-
-// Remove element from the end of the array
-
-// Add element at the beginning of the array
-
-// Remove element from the beginning of the array
-
-// Split the string into an array
-
-// Combine array elements into string
-
-// Check if element exist in the array
-
-// Search element index in the array
-
-// Merge two arrays
-
-// Sorting of array (Reverse order also)
-
-// Filter, map, reduce of array
+// // Append element
+// $fruts[]="peach";
 
 
-// https://www.php.net/manual/en/ref.array.php
+// // Print the length of the array
+// //echo count($fruts).'<br>';
+// // Add element at the end of the array
+
+// // Remove element from the end of the array
+
+// // Add element at the beginning of the array
+
+// // Remove element from the beginning of the array
+
+// // Split the string into an array
+
+// // Combine array elements into string
+
+// // Check if element exist in the array
+// //in_array('Apple',$fruts);
+// // Search element index in the array
+// //echo array_search('apple',$fruts);
+// // Merge two arrays
+
+// // Sorting of array (Reverse order also)
+// //sort($fruts);
+// //rsort($fruts);
+// // Filter, map, reduce of array
+
+// $numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+// $evens = array_filter($numbers, fn($n) => $n % 2 === 0);
+
+
+
+
+
+// echo '<pre>';
+//  var_dump($evens);
+//  echo '</pre>';
+// // https://www.php.net/manual/en/ref.array.php
+
+// $squares = array_map(fn($n) => $n * $n, $numbers);
+
+// echo '<pre>';
+//  var_dump($squares);
+//  echo '</pre>';
 
 // ============================================
 // Associative arrays
 // ============================================
 
 // Create an associative array
+
+$person = [
+    'name' => 'Brad',
+    'surname' => 'Traversy',
+    'age' => 30,
+    'hobbies' => ['Tennis', 'Video Games']
+];
+
+
+echo $person['name'];
 
 // Get element by key
 

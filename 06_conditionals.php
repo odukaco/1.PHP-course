a879a1aa44a73d4e8a808dad8349d436b0239c39
dcosovan <david.cosovan@leviatan.ro>
2022-10-21 12:41:27 +0300

    1:52:21

--- 06_conditionals.php
+++ 06_conditionals.php
@@ -1,6 +1,6 @@
 <?php
 
-$age = 20;
+$age = 60;
 $salary = 300000;
 
 // if condition
@@ -12,14 +12,37 @@ $salary = 300000;
 // if condition1 OR condition2
 
 // if condition1 - elseif condition2 - else
-
+// if($age < 20)
+// {
+//     echo 'you are young<br>';
+// }elseif ($age<30)
+// {
+//     echo 'you are not really old<br>';
+// }
 // if condition1 and condition2 - elseif condition1 and condition2 - else
 
 // Ternary if
-
+//echo $age < 22 ? 'young' : ($age<50 ? 'not old':'old'),'<br>';
 // Null coalescing operator
-
+$MyAge = $age ?? 18;
+echo $MyAge.'<br>';
+$MyAddress = $address ?? 'current location';
+echo $MyAddress.'<br>';
 // Null coalescing assignment operator. Since PHP 7.4
-
+$person = ['name' => 'jhon'];
+$person ['name'] ??='Anonymous';
+echo $person['name'].'<br>';
 // switch
+$userRole = 'admin';
+switch($userRole)
+{
+    case 'admin':
+        echo 'you are admin'.'<br>';
+        break;
+    case 'editor':
+        echo 'you are editor'.'<br>';
+        break;
+    default:
+        echo 'Unknown'.'<br>';
+}
 

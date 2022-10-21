8b27d4d92e5530c74eabb668d284c0864a5f4290
dcosovan <david.cosovan@leviatan.ro>
2022-10-21 13:40:40 +0300

    2:01:31

--- 07_loops.php
+++ 07_loops.php
@@ -1,5 +1,5 @@
 <?php
-
+//$i =0;
 // while
 
 // Loop with $counter
@@ -7,7 +7,25 @@
 // do - while
 
 // for
-
+// for ( ; $i<10; $i++)
+// {
+//     echo "Counter : $i".'<br>';
+// }
 // foreach
-
+//  $fruits = ['banana','apple','orange'];
+//  foreach($fruits as $i => $fruit)
+//  {
+//     echo $i.' '.$fruit.'<br>';
+//  }
 // Iterate Over associative array.
+$person =[
+    'name' => 'travers',
+    'surname' => 'Bob',
+    'age' => 30,
+    'hobbies' => ['tennis','run']
+];
+foreach($person as $index => $valoare)
+{
+    if ($index === 'hobbies')break;
+    echo $valoare .'<br>';
+}
--- 08_functions.php
+++ 08_functions.php
@@ -1,11 +1,30 @@
 <?php
 
 // Function which prints "Hello I am TheCodeholic"
-
+// function hello($name)
+// {
+//     echo 'Hello I am '.$name.'<br>';
+// }
+// hello('name');
 // Create sum of two functions
+// function sum($a,$b)
+// {
+//     echo $a + $b.'<br>';
+// }
 
+// sum(3,5);
 // Create function to sum all numbers using ...$nums
 
+function sum(...$numbers)
+{
+    $sum=0;
+    foreach($numbers as $num)
+    {
+        $sum+=$num;
+    }
+    return $sum;
+}
+echo sum(2,2,3);
 // Arrow functions
 
 

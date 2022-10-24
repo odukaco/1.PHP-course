37494cf4e83484a1060217d7e6565695045745ce
dcosovan <david.cosovan@leviatan.ro>
2022-10-24 15:12:29 +0300

    3:12:42

--- 10_superglobals/03_file_uploading/file_upload.php
+++ 10_superglobals/03_file_uploading/file_upload.php
@@ -1,12 +1,24 @@
 <?php
-
-echo '<pre>';
-var_dump($_FILES);
-echo '</pre>';
+$errorMessage = '';
+// echo '<pre>';
+// var_dump($_FILES);
+// echo '</pre>';
 
 if(isset($_FILES['file'])){
-  move_uploaded_file($_FILES['file']['tmp_name'], $_FILES['file']['name']);
+  $file = $_FILES['file'];
+  $ext = pathinfo($file['name'], PATHINFO_EXTENSION);
+  $ext = strtolower($ext);
+  if($file['size'] > 5*1024*1024){
+    $errorMessage = 'you can not upload more than 5mb';
+  }elseif (!in_array($ext, ['png','jpeg','svg','jpg'])){
+    $errorMessage = 'you can only upload images';
+  }else{
+    move_uploaded_file($_FILES['file']['tmp_name'], $_FILES['file']['name']);
+  }
+
+
 }
+
 
 ?>
 <!doctype html>
@@ -19,6 +31,9 @@ if(isset($_FILES['file'])){
   <title>Document</title>
 </head>
 <body>
+  <p>
+    <?php echo $errorMessage ?>
+  </p>
 <form action="" method="post" enctype="multipart/form-data">
   <input type="file" name="file"><br>
   <button>Submit</button>
